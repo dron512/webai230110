8ad9eb9faefe60a5633bce65fed73e87129b6787
dron512 <parkmyounghoi@gmail.com>
2023-01-18 15:35:44 +0900

    230118

---
+++ flask_work/20230118/freeboard.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+import config
+
+prefix = 'freeboard'
+
+app = Blueprint(prefix,__name__, url_prefix=f'/{prefix}')
+
+@app.route("insertform")
+def insertform():
+    return f'{prefix}/insertform'
+
+@app.route("insertproc")
+def insertproc():
+    return f'{prefix}/insertproc'
+
+@app.route("select")
+def select():
+    pageNum = request.args.get('pageNum')
+    print(pageNum)
+    '''
+        pageNum None
+        pageNum 1
+        pageNum 2
+    '''
+    if pageNum == None:
+        pageNum = 1
+    else:
+        pageNum = int(pageNum)
+
+    '''
+        1 => 0   => (1-1)*3 =0
+        2 => 3   => (2-1)*3 = 3
+        3 => 6   => (3-1)*3 = 6
+        4 => 9   => (4-1)*3 = 9
+    '''
+    connection = config.connect()
+    cursor = connection.cursor()
+    sql = f'select * from freeboard ORDER BY idx DESC LIMIT {((pageNum -1)*3)},3'
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    config.close(connection)
+    return render_template(f'{prefix}/select.html',res=res)
