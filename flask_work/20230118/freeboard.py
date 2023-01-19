0ce3f5b589f00c97458390571955c01bf9d28039
dron512 <parkmyounghoi@gmail.com>
2023-01-19 11:21:41 +0900

    230119

--- flask_work/20230118/freeboard.py
+++ flask_work/20230118/freeboard.py
@@ -16,28 +16,23 @@ def insertproc():
 @app.route("select")
 def select():
     pageNum = request.args.get('pageNum')
-    print(pageNum)
-    '''
-        pageNum None
-        pageNum 1
-        pageNum 2
-    '''
     if pageNum == None:
         pageNum = 1
     else:
         pageNum = int(pageNum)
-
-    '''
-    모르고 메시지 안남기고 커밋
-        1 => 0   => (1-1)*3 =0
-        2 => 3   => (2-1)*3 = 3
-        3 => 6   => (3-1)*3 = 6
-        4 => 9   => (4-1)*3 = 9
-    '''
     connection = config.connect()
     cursor = connection.cursor()
     sql = f'select * from freeboard ORDER BY idx DESC LIMIT {((pageNum -1)*3)},3'
     cursor.execute(sql)
     res = cursor.fetchall()
     config.close(connection)
+
+    connection = config.connect()
+    cursor = connection.cursor()
+    sql = f'select count(idx) from freeboard'
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    print('cnt = ',res[0])
+    config.close(connection)
+
     return render_template(f'{prefix}/select.html',res=res)
