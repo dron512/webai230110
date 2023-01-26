03c8ebf796d8b1f9d1cfded6560c887276fb0685
dron512 <parkmyounghoi@gmail.com>
2023-01-26 15:25:44 +0900

    230126

---
+++ flask_work/homepage/components/db/freeboardmanage.py
@@ -0,0 +1,13 @@
+import pymysql
+
+def select():
+    print('select해야함')
+
+def insert():
+    print("insert해야함")
+
+def delete():
+    print("delete해야함")
+
+def update():
+    print("update 수정해야함")
--- flask_work/homepage/web.py
+++ flask_work/homepage/web.py
@@ -1,9 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from components import freeboard
+from components import fileboard
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
+app.register_blueprint(freeboard.app)
+app.register_blueprint(fileboard.app)
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    x = request.args.get('x')
+    y = '예측되는숫자를 넣으셔야 합니다.'
+    print('x = ',x)
+    if x :
+        knr = LinearRegression()
+        knr.fit(
+            [[28],[29],[30],[31],[32]],
+            [200,220,240,260,280]
+        )
+        y = knr.predict([[float(x)]])
+    return render_template('index.html',y=y)
 
 app.run(debug=True)
