699608447b65438e0e95c740139ae102c2f617d8
dron512 <parkmyounghoi@gmail.com>
2023-02-01 10:30:07 +0900

    230201

--- js_work/myapp/src/App.js
+++ js_work/myapp/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>타이틀</h1>
-
+      <p>
+        lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem 
+      </p>
+      <div>
+        안녕하세요 홍길동입니다.
+      </div>
     </div>
   );
 }
