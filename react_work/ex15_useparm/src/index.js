838de60a67303887fc2eecd2f262df6fa0d9364a
dron512 <parkmyounghoi@gmail.com>
2023-04-03 11:40:23 +0900

    230403

--- react_work/ex15_useparm/src/index.js
+++ react_work/ex15_useparm/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App1 from './App1';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <App />
+  // <App />
+  <BrowserRouter>
+    <App1 />
+  </BrowserRouter>
   // </React.StrictMode>
 );
 
